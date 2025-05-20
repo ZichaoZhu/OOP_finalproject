b91e66b63447e77a152ba3a3571455e14f8cba70
ZichaoZhu <1606783699@qq.com>
2025-05-20 20:47:18 +0800

    配置完了launch.json文件，以及tasks .json文件，实现了binary部分的运行自动化以及调试自动化

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zzc/Desktop/OOP homework/Finalproject
+# Build directory: /Users/zzc/Desktop/OOP homework/Finalproject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BinaryTest "/Users/zzc/Desktop/OOP homework/Finalproject/build/bin/binary_test")
+set_tests_properties(BinaryTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/zzc/Desktop/OOP homework/Finalproject/CMakeLists.txt;26;add_test;/Users/zzc/Desktop/OOP homework/Finalproject/CMakeLists.txt;0;")
+subdirs("external/googletest")
---
+++ build/external/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/zzc/Desktop/OOP homework/Finalproject/external/googletest/googlemock
+# Build directory: /Users/zzc/Desktop/OOP homework/Finalproject/build/external/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
