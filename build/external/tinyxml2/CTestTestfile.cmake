a922b85dc28c4234fa3251380e81fd6cc285e479
ZichaoZhu <1606783699@qq.com>
2025-05-25 18:33:32 +0800

    创建release版本

--- build/external/tinyxml2/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zzc/Desktop/OOP homework/Finalproject/external/tinyxml2
-# Build directory: /Users/zzc/Desktop/OOP homework/Finalproject/build/external/tinyxml2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(xmltest "/Users/zzc/Desktop/OOP homework/Finalproject/build/bin/xmltest")
-set_tests_properties(xmltest PROPERTIES  PASS_REGULAR_EXPRESSION ", Fail 0" WORKING_DIRECTORY "/Users/zzc/Desktop/OOP homework/Finalproject/external/tinyxml2" _BACKTRACE_TRIPLES "/Users/zzc/Desktop/OOP homework/Finalproject/external/tinyxml2/CMakeLists.txt;49;add_test;/Users/zzc/Desktop/OOP homework/Finalproject/external/tinyxml2/CMakeLists.txt;0;")
